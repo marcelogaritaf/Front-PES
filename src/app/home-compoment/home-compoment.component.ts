4e9e5f6a4348d15201a48171b5b648f337335c96
Marcelo Garita <mgaritaf@outlook.com>
2023-10-09 18:54:46 -0600

    home compoment

---
+++ src/app/home-compoment/home-compoment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-compoment',
+  templateUrl: './home-compoment.component.html',
+  styleUrls: ['./home-compoment.component.css']
+})
+export class HomeCompomentComponent {
+
+}
