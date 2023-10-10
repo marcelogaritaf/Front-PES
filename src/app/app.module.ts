139a954422de74f13dc6320e500b56812ab42587
Marcelo Garita <mgaritaf@outlook.com>
2023-10-09 18:55:37 -0600

    creacion de menu y estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeCompomentComponent } from './home-compoment/home-compoment.component';
+import { RouterModule, Routes } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http'
+const sistemaRoutes:Routes=[
+  {path:'',component:HomeCompomentComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeCompomentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule.forRoot(sistemaRoutes),//indica las rutas del sistema
+
+
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
