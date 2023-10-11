e9f19689d532fcaaa73b5f731a222c7db3224ed1
Marcelo Garita <mgaritaf@outlook.com>
2023-10-10 18:22:16 -0600

    cambios varios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeCompomentComponent } from './home-compoment/home-compoment.component';
 import { RouterModule, Routes } from '@angular/router';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { ArticulosComponent } from './articulos/articulos.component';
+import { AsignarComponent } from './asignar/asignar.component';
+import { OficinasComponent } from './oficinas/oficinas.component';
+import { DesignadasComponent } from './designadas/designadas.component';
+import { RegistroAsignarComponent } from './home-compoment/registro-asignar/registro-asignar.component';
+
+
 const sistemaRoutes:Routes=[
-  {path:'',component:HomeCompomentComponent}
+  {path:'',component:HomeCompomentComponent},
+  {path:'Articulos', component:ArticulosComponent},
+  {path:'Asignacion', component:AsignarComponent},
+  {path:'Oficinas', component:OficinasComponent},
+  {path:'Designadas', component:DesignadasComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeCompomentComponent
+    HomeCompomentComponent,
+    ArticulosComponent,
+    AsignarComponent,
+    OficinasComponent,
+    DesignadasComponent,
+    RegistroAsignarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, RouterModule.forRoot(sistemaRoutes),//indica las rutas del sistema
-
 
     HttpClientModule
   ],
