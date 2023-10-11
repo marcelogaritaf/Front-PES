ff68b0397b577b754ac24ea228cb19fcc84ead43
Marcelo Garita <mgaritaf@outlook.com>
2023-10-10 18:20:43 -0600

    oficinas component

---
+++ src/app/oficinas/oficinas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oficinas',
+  templateUrl: './oficinas.component.html',
+  styleUrls: ['./oficinas.component.css']
+})
+export class OficinasComponent {
+
+}
